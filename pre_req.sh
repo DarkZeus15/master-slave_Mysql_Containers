1c8043e2b22b38441be82d01366f32cd45b56daf
Ajinkya <ajinkya.borade1507@gmail.com>
2024-01-12 18:11:08 +0530

    Working content updates

--- pre_req.sh
+++ pre_req.sh
@@ -1,38 +1,40 @@
-mkdir -p mysql-master-slave/master/conf/
-mkdir -p mysql-master-slave/slave/conf/
+mkdir -p mysql-master-slave_1/master/conf/
+mkdir -p mysql-master-slave_1/slave/conf/
 
-echo "Creating File : mysql-master-slave/slave/conf/mysql.conf.cnf"
+echo "Creating File : mysql-master-slave_1/master/conf/mysql.conf.cnf"
 echo '''[mysqld]
 skip-name-resolve
 default_authentication_plugin = mysql_native_password
 server-id = 1
 log_bin = 1
 binlog_format = ROW
-binlog_do_db = mydb''' > mysql-master-slave/slave/conf/mysql.conf.cnf
+binlog_do_db = mydb''' > mysql-master-slave_1/master/conf/mysql.conf.cnf
 
 
-echo "Creating file : mysql-master-slave/slave/conf/mysql.conf.cnf"
+echo "Creating file : mysql-master-slave_1/slave/conf/mysql.conf.cnf"
 echo '''[mysqld]
 skip-name-resolve
 default_authentication_plugin = mysql_native_password
 server-id = 2
 log_bin = 1
-binlog_do_db = mydb''' > mysql-master-slave/slave/conf/mysql.conf.cnf
+binlog_do_db = mydb''' > mysql-master-slave_1/slave/conf/mysql.conf.cnf
 
 
-echo "Creating File : mysql-master-slave/master/mysql_master.env"
+echo "Creating File : mysql-master-slave_1/master/mysql_master.env"
 echo '''MYSQL_ROOT_PASSWORD=111
 MYSQL_PORT=3306
 MYSQL_USER=mydb_user
 MYSQL_PASSWORD=mydb_pwd
 MYSQL_DATABASE=mydb
-MYSQL_LOWER_CASE_TABLE_NAMES=0''' > mysql-master-slave/slave/mysql_master.env
+MYSQL_LOWER_CASE_TABLE_NAMES=0''' > mysql-master-slave_1/master/mysql_master.env
 
 
-echo "Creating File : mysql-master-slave/slave/mysql_slave.env"
+echo "Creating File : mysql-master-slave_1/slave/mysql_slave.env"
 echo '''MYSQL_ROOT_PASSWORD=111
 MYSQL_PORT=3306
 MYSQL_USER=mydb_slave_user
 MYSQL_PASSWORD=mydb_slave_pwd
 MYSQL_DATABASE=mydb
-MYSQL_LOWER_CASE_TABLE_NAMES=0''' > mysql-master-slave/slave/mysql_slave.env
+MYSQL_LOWER_CASE_TABLE_NAMES=0''' > mysql-master-slave_1/slave/mysql_slave.env
+
+cd mysql-master-slave_1
